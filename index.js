4af4e55dcf64e1ebf8102d7e65520971a43eb74b
Deborah Maciel <104872371+debysdm@users.noreply.github.com>
2023-09-26 20:42:49 -0300

    novo site

--- index.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('button')
-const navigation = document.querySelector('.navigation')
-
-function handleClick() {
-  button.classList.toggle('active')
-  navigation.classList.toggle('active')
-}
